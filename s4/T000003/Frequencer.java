9610552c4a0f3b4eb214d098a4dc013bdcf414c7
Shiori Hironaka <noahluces@gmail.com>
2020-02-04 15:48:38 +0900

    Remove comments

--- s4/T000003/Frequencer.java
+++ s4/T000003/Frequencer.java
@@ -2,19 +2,6 @@ package s4.T000003; // Please modify to s4.Bnnnnnn, where nnnnnn is your student
 import java.lang.*;
 import s4.specification.*;
 
-/*
-interface FrequencerInterface {  // This interface provides the design for frequency counter.
-    void setTarget(byte[] target);  // set the data to search.
-    void setSpace(byte[] space);  // set the data to be searched target from.
-    int frequency(); // It return -1, when TARGET is not set or TARGET's length is zero
-                     // Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                     // Otherwise, get the frequency of TAGET in SPACE
-    int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e. target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
-}
-*/
-
 
 class SimpleFrequencer implements FrequencerInterface {
     // Code to Test, *warning: This code contains intentional problem*
